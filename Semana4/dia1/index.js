9969641f40b689d478968d1dadf3d5e633f30990
villavant <jorge.villavicencio10@gmail.com>
2021-03-15 20:27:25 -0500

    example of javascripts

---
+++ Semana4/dia1/index.js
@@ -0,0 +1,31 @@
+// Variables de tipo texto
+let nombre = "Josue";
+
+// Variable de tipo número 
+let edad = 24
+let altura = 1.67
+let peso = 66.9
+
+let a = 5;
+let b = 10;
+// bmi = indice de masa corporal
+let bmi = (peso) / (altura ** 2)
+
+alert(bmi);
+
+let suma = a + b;
+console.log(suma);
+
+let resta = a - b;
+console.log(resta);
+
+let multiplicacion = a * b;
+console.log(a * b);
+let modulo = a % b;
+console.log(modulo);
+
+let potenciacion = a ** b;
+console.log(potenciacion);
+
+let radicacion = a ** 0.5;
+console.log(radicacion);
