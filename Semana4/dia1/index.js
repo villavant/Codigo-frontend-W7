0892a95e40d4052904ae2e35cdf6c9ac609e20a8
villavant <jorge.villavicencio10@gmail.com>
2021-03-15 22:20:16 -0500

    using variables with the cuadrante form

--- Semana4/dia1/index.js
+++ Semana4/dia1/index.js
@@ -1,31 +1,65 @@
-// Variables de tipo texto
-let nombre = "Josue";
+// // Variables de tipo texto
+// let nombre = "Josue";
 
-// Variable de tipo número 
-let edad = 24
-let altura = 1.67
-let peso = 66.9
+// // Variable de tipo número 
+// let edad = 24
+// let altura = 1.67
+// let peso = 66.9
 
-let a = 5;
-let b = 10;
-// bmi = indice de masa corporal
-let bmi = (peso) / (altura ** 2)
+// let a = 5;
+// let b = 10;
+// // bmi = indice de masa corporal
+// let bmi = (peso) / (altura ** 2)
 
-alert(bmi);
+// alert(bmi);
 
-let suma = a + b;
-console.log(suma);
+// let suma = a + b;
+// console.log(suma);
 
-let resta = a - b;
-console.log(resta);
+// let resta = a - b;
+// console.log(resta);
 
-let multiplicacion = a * b;
-console.log(a * b);
-let modulo = a % b;
-console.log(modulo);
+// let multiplicacion = a * b;
+// console.log(a * b);
+// let modulo = a % b;
+// console.log(modulo);
 
-let potenciacion = a ** b;
-console.log(potenciacion);
+// let potenciacion = a ** b;
+// console.log(potenciacion);
 
-let radicacion = a ** 0.5;
-console.log(radicacion);
+// let radicacion = a ** 0.5;
+// console.log(radicacion);
+
+
+// let input = 5;
+// let celsius = (input -32)*(5/9);
+
+// console.log("F a Celsius: ",celsius );
+
+let jorge = {
+    nombre: "Jorge",
+    apellido: "Villavicencio",
+    edad: 27
+}
+
+console.log(`Descripción ${jorge.nombre} 
+${jorge.apellido}
+${jorge.edad}`);
+
+let user = {
+    hobbieFavorito: "Escuchar música",
+    nroMascotas: 3,
+    flagHombre: true
+}
+
+console.log(user);
+alert(`Bienveido ${jorge.nombre}`);
+
+let a = 1;
+let b = 2;
+let c = 1;
+let result1 = ((-b) + ((((b ** 2) - (4 * a * c)) ** 0.5))) / (2 * a);
+let result2 = ((-b) - ((((b ** 2) - (4 * a * c)) ** 0.5))) / (2 * a);
+
+console.log(`x1  ${result1} 
+x2  ${result2} `);
